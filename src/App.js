ad6cb81f441658183b02c5a92c9c96d310d4d6a1
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-05-03 19:16:20 +0100

    can serch by tag and name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 
 
@@ -8,15 +8,20 @@ function App() {
 
   const [images, setImages] = useState([])
   const [input, setInput] = useState("")
-   
+  const [value, setValue] = useState('name');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
 
   useEffect(() => {
     fetch("/api/images")
-    .then(response => response.json())
-    .then(data => {
-      setImages(data);
-     })
-  }, [] )
+      .then(response => response.json())
+      .then(data => {
+        setImages(data);
+      })
+  }, [])
 
   const handleInoutChange = (e) => {
     setInput(e.target.value)
@@ -26,24 +31,49 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <label>
+          Serch type
+          <select value={value} onChange={handleChange}>
+            <option value="name">Name</option>
+            <option value="tag">Tag</option>
+          </select>
+        </label>
 
-          <input type="text" onChange={handleInoutChange} />
+        <p>{value === 'name' &&
+          <h2>
+            <input type="text" onChange={handleInoutChange} />
+            {
+              images
+                .filter(image => image && image.Name_of_image.includes(input))
+                .map((image) =>
+                  <div>
+                    <p>name: {image.Name_of_image}</p>
+                    <p>tag: {image.tag}</p>
+                    <p>id: {image.id}</p>
+                    <img width="200" src={image.HTML_URL} />
+                  </div>
+                )
+            }
+          </h2>
+        }</p>
+        <p>{value === 'tag' &&
+          <h2>
+            <input type="text" onChange={handleInoutChange} />
+            {
+              images
+                .filter(image => image && image.tag.includes(input))
+                .map((image) =>
+                  <div>
+                    <p>name: {image.Name_of_image}</p>
+                    <p>tag: {image.tag}</p>
+                    <p>id: {image.id}</p>
+                    <img width="200" src={image.HTML_URL} />
+                  </div>
+                )
+            }
+          </h2>
+        }</p>
 
-          {
-            images
-              .filter(image => image && image.Name_of_image.includes(input))      
-              .map((image) => 
-                <div>
-                  <p>id: {image.id}</p>
-                  <img width="200" src={image.HTML_URL}/>
-                  <p>name: {image.Name_of_image}</p>
-                  <p>name: {image.tag}</p>
-                </div> 
-              )
-          }
-
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
