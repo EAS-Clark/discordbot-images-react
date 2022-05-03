113fe90f42c15c5f698b5e792f3c1e972fc651cb
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-05-03 17:37:58 +0100

    added fetch images with filter

--- src/App.js
+++ src/App.js
@@ -2,16 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
 
+
+
 function App() {
+
+  const [images, setImages] = useState([])
+  const [input, setInput] = useState("")
+   
+
   useEffect(() => {
     fetch("/api/images")
+    .then(response => response.json())
+    .then(data => {
+      setImages(data);
+     })
   }, [] )
+
+  const handleInoutChange = (e) => {
+    setInput(e.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+          <input type="text" onChange={handleInoutChange} />
+
+          {
+            images
+              .filter(image => image && image.Name_of_image.includes(input))      
+              .map((image) => 
+                <div>
+                  <p>id: {image.id}</p>
+                  <img width="200" src={image.HTML_URL}/>
+                  <p>name: {image.Name_of_image}</p>
+                  <p>name: {image.tag}</p>
+                </div> 
+              )
+          }
+
         </p>
         <a
           className="App-link"
