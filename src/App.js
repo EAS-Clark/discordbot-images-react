e156a61973cc038cb7546a3be226267a5fe3b2d8
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-05-04 12:21:22 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+function MyForm() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs.Name_of_image + " " +  inputs.HTML_URL + " " + inputs.tag);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter image HTML_URL:
+      <input 
+        type="text" 
+        name="HTML_URL" 
+        value={inputs.HTML_URL || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <label>Enter image name:
+        <input 
+          type="text" 
+          name="Name_of_image" 
+          value={inputs.Name_of_image || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <label>Enter image tag:
+        <input 
+          type="text" 
+          name="tag" 
+          value={inputs.tag || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <input type="submit" />
+    </form>
+    
+  )
+}
 
 
 
@@ -8,7 +57,7 @@ function App() {
 
   const [images, setImages] = useState([])
   const [input, setInput] = useState("")
-  const [value, setValue] = useState('name');
+  const [value, setValue] = useState('Name_of_image');
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -31,20 +80,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+
+
+        </p>
         <label>
           Serch type
           <select value={value} onChange={handleChange}>
-            <option value="name">Name</option>
+            <option value="Name_of_image">Name</option>
             <option value="tag">Tag</option>
           </select>
         </label>
-
-        <p>{value === 'name' &&
+        <p>{
           <h2>
             <input type="text" onChange={handleInoutChange} />
             {
               images
-                .filter(image => image && image.Name_of_image.includes(input))
+                .filter(image => image && image[value].includes(input))
                 .map((image) =>
                   <div>
                     <p>name: {image.Name_of_image}</p>
@@ -56,24 +108,6 @@ function App() {
             }
           </h2>
         }</p>
-        <p>{value === 'tag' &&
-          <h2>
-            <input type="text" onChange={handleInoutChange} />
-            {
-              images
-                .filter(image => image && image.tag.includes(input))
-                .map((image) =>
-                  <div>
-                    <p>name: {image.Name_of_image}</p>
-                    <p>tag: {image.tag}</p>
-                    <p>id: {image.id}</p>
-                    <img width="200" src={image.HTML_URL} />
-                  </div>
-                )
-            }
-          </h2>
-        }</p>
-
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -87,4 +121,5 @@ function App() {
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+export default MyForm;
